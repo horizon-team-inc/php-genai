238d034af4c5df89be92a970363a30b337679e87
horizon-team-inc <horizon.team.toronto@gmail.com>
2025-06-25 12:34:23 -0400

    [ADD] Create HttpClient class for API interactions

---
+++ src/Http/HttpClient.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace HorizonLLC\PhpGenAI\Http;
+
+use GuzzleHttp\Client as GuzzleClient;
+
+class HttpClient
+{
+    protected GuzzleClient $client;
+    protected string $apiKey;
+
+    public function __construct(string $apiKey) {
+        $this->apiKey = $apiKey;
+        $this->client = new GuzzleClient([
+            'base_uri' => "https://generativelanguage.googleapis.com/v1beta/",
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ]
+        ]);
+    }
+}
